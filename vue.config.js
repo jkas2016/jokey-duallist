89aea20cdce7bd0d16a39b483fc8b330c92f01d0
Yeon Ik Jo <whdusdlr2015@gmail.com>
2021-05-28 17:24:19 +0900

    vuetify treeshaking 소스 수정

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,7 @@
 const path = require( "path" );
 
-const VuetifyLoaderPlugin = require( 'vuetify-loader/lib/plugin' );
-
 module.exports = {
     configureWebpack: {
-        plugins: [
-            new VuetifyLoaderPlugin()
-        ],
         resolve: {
             alias: {
                 "@": path.join( __dirname, 'src/' ),
